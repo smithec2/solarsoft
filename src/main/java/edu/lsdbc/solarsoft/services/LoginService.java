9fef372a748a3196e28e6536259720ba117e61a1
smithec2 <gobionx@gmail.com>
2016-12-01 10:47:27 -0700

    new login page

---
+++ src/main/java/edu/lsdbc/solarsoft/services/LoginService.java
@@ -0,0 +1,11 @@
+package edu.lsdbc.solarsoft.services;
+
+/**
+ * Created by XPS on 12/1/2016.
+ */
+public class LoginService {
+    public boolean haveUsername(String userName) {
+
+        return true;
+    }
+}
---
+++ src/main/java/edu/lsdbc/solarsoft/views/Login.java
@@ -0,0 +1,32 @@
+package edu.lsdbc.solarsoft.views;
+
+import edu.lsdbc.solarsoft.services.HomepageService;
+import edu.lsdbc.solarsoft.services.LoginService;
+
+import java.util.Scanner;
+
+/**
+ * Created by XPS on 12/1/2016.
+ */
+public class Login implements BaseView {
+    LoginService loginService = new LoginService();
+    HomepageService homepageService = new HomepageService();
+    public void display() {
+        Scanner input = new Scanner(System.in);
+        String userName = null;
+        while (!loginService.haveUsername(userName)) {
+            System.out.println("Enter User Name");
+            userName = input.next();
+        }
+
+        // go to the home page
+        homepageService.goHome(userName);
+
+
+        loginService.haveUsername(userName);
+
+
+
+
+    }
+}

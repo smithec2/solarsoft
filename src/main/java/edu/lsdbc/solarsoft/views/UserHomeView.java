41bb11476ac9988a886e1adde6882557a59a8a76
ArthurTWelch <arthurtwelch@gmail.com>
2016-12-06 11:06:58 -0700

    Arthur's changes

---
+++ src/main/java/edu/lsdbc/solarsoft/views/UserHomeView.java
@@ -0,0 +1,15 @@
+package edu.lsdbc.solarsoft.views;
+import java.util.Scanner;
+/**
+ * Created by Arthur on 12/6/2016.
+ */
+public class UserHomeView {
+    //public void display(); Use?
+
+    // This will be what Users will interface with
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+    }
+}

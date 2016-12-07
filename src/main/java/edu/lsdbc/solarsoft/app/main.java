92526cb56953da0c26f3ba561c90f1434ed029db
smithec2 <gobionx@gmail.com>
2016-12-06 22:32:22 -0700

    add app/main.class to start the program

---
+++ src/main/java/edu/lsdbc/solarsoft/app/main.java
@@ -0,0 +1,13 @@
+package edu.lsdbc.solarsoft.app;
+
+import edu.lsdbc.solarsoft.views.Login;
+
+/**
+ * Created by XPS on 12/6/2016.
+ */
+public class main {
+    public static void main(String [] args) {
+        Login login = new Login();
+        login.display();
+    }
+}

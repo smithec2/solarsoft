276bd31b8edf8736940887349142025fa3c872ea
ArthurTWelch <arthurtwelch@gmail.com>
2016-12-07 22:27:18 -0700

    Arthur's changes

--- src/main/java/edu/lsdbc/solarsoft/models/Attachment1.java
+++ src/main/java/edu/lsdbc/solarsoft/models/Attachment1.java
@@ -4,4 +4,24 @@ package edu.lsdbc.solarsoft.models;
  * Created by Arthur on 12/7/2016.
  */
 public class Attachment1 {
+
+    // Attributes
+    //PropertyOwner propertyOwner = new PropertyOwner();  -- need class
+    //Contractor contractor = new Contractor();           -- need default constructor
+    boolean feePaid;
+    boolean sitePlanApproved;
+    boolean oneLineDiagram;
+    boolean engineerLetter;
+
+    boolean solarPacketForms;
+    boolean packetQuestions;
+    boolean packetMetering;
+    boolean packetLightPower;
+    boolean packetBuildingPermit;
+
+    boolean specSheets;
+    boolean electricalPhoto;
+
+    //DigitalSignature signOwner1 = new DigitalSignature;  -- need class
+    //DigitalSignature signOwner2 = new DigitalSignature;  -- need class
 }

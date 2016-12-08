ab955ec5210caa0c31199e142331b6b8350cbe4d
sakano1 <sakano1@ldsbc.edu>
2016-12-08 11:23:11 -0700

    Admin view

---
+++ src/main/java/edu/lsdbc/solarsoft/services/ApplicationService.java
@@ -0,0 +1,29 @@
+package edu.lsdbc.solarsoft.services;
+
+/**
+ * Created by Scott on 12/8/2016.
+ */
+public class ApplicationService {
+    public void viewAllSubmitedApplications(){
+
+    }
+    public void viewUserApplication(String userName, int appID){
+
+    }
+    public void newApplication(){
+
+    }
+    public void editApplication(String userName, int appID) {
+
+    }
+    public void deleteApplication(String userName, int appID){
+
+    }
+    public void submitApplication(String userName, int appID){
+
+    }
+    public void acceptApplication(String userName, int appID){
+
+    }
+
+}

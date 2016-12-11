96fa68d6f1106f71a20ead9f73c20a4548c84dbd
smithec2 <gobionx@gmail.com>
2016-12-10 22:25:25 -0700

    added dao fixed login

---
+++ src/main/java/edu/lsdbc/solarsoft/views/UnableToLoginView.java
@@ -0,0 +1,16 @@
+package edu.lsdbc.solarsoft.views;
+
+/**
+ * Created by XPS on 12/10/2016.
+ */
+public class UnableToLoginView extends BaseView {
+
+    public void display() {
+        clearDisplay();
+        System.out.println("Sorry User Name and Password do not match");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        new Login().display();
+    }
+}

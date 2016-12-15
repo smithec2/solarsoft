8e8d05f52178c8ffb593a81e235adf8401ca331a
sakano1 <sakano1@ldsbc.edu>
2016-12-14 21:14:59 -0700

    Modified the login page to have a menu and add EnterCredentialsView

--- src/main/java/edu/lsdbc/solarsoft/services/HomepageService.java
+++ src/main/java/edu/lsdbc/solarsoft/services/HomepageService.java
@@ -1,30 +1,42 @@
 package edu.lsdbc.solarsoft.services;
 
+import edu.lsdbc.solarsoft.dao.UserDAO;
 import edu.lsdbc.solarsoft.models.UserType;
 import edu.lsdbc.solarsoft.views.AdminHome;
+import edu.lsdbc.solarsoft.views.CityWorkHomeView;
 import edu.lsdbc.solarsoft.views.UserHome;
 
 /**
  * Created by XPS on 12/1/2016.
+ *
+ * Edited by: Scott Muhlestein - SM on 12/14/2016
  */
 public class HomepageService {
     public void goHome(String userName) {
-        switch (findUserType(userName)){
+        // SM - UserDAO object to access the database
+        UserDAO findUserType = new UserDAO();
+
+        // SM - Use UserDAO to find UserType
+        switch (findUserType.findUserType(userName)) {
             case applicant:
                 new UserHome().display(userName);
-            break;
+                break;
             case admin:
                 new AdminHome().display(userName);
                 break;
             case employ:
-                //EmployHome
+                new CityWorkHomeView().display(userName);
                 break;
         }
 
 
     }
 
+
+    /*
+    // Commented out By Scott
     private UserType findUserType(String userName) {
         return UserType.applicant;
     }
+    */
 }
